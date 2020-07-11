a3dd95373d5f6b23529e198505ec352e21b6b3b9
swengkie88 <angkiprana1987@gmail.com>
2020-07-11 13:49:20 +0700

    commit pertama nih ye

---
+++ README.txt
@@ -0,0 +1,26 @@
+Rencana Struktur web kmm ft
+- Beranda
+ -> Tentang + intro (video)?
+ -> Jadwal Sholat
+ -> info struktur
+ -> agenda
+ -> artikel
+ -> kontak
+
+- Profil
+ -> visi misi
+ -> struktur organisasi
+ -> sejarah
+
+- pondasi islam
+ -> syariah
+ -> akhlak
+ -> aqidah
+ -> ibadah
+
+- Syiar
+ -> Miracle of Al Quran
+ -> Muallaf
+
+- artikel
+- kontak/agenda?
---
+++ assets/js/main.js
@@ -0,0 +1,46 @@
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = [
+    "Man Jadda Wa Jadda",
+    // Siapa yang bersungguh sungguh akan berhasil
+    "Man Shabara Zhafira",
+    // Siapa yang sabar akan beruntung
+    "Man Sara Ala Darbi Washala"
+    // Siapa yang berjalan di jalur-Nya akan sampai 
+];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+        if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    } else {
+        cursorSpan.classList.remove("typing");
+        setTimeout(erase, newTextDelay);
+    }
+}
+
+function erase() {
+    if (charIndex > 0) {
+        if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    } else {
+        cursorSpan.classList.remove("typing");
+        textArrayIndex++;
+        if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+        setTimeout(type, typingDelay + 1100);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+    if (textArray.length) setTimeout(type, newTextDelay + 250);
+});
